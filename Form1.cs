52ca4af31633073fa2517a7a87baa1818cdfdc1c
Oliver <ocben1@gmail.com>
2020-01-24 07:16:50 +1100

    using class SignalGenerator

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,21 @@ namespace MusicalInstrument
 {
     public partial class Form1 : Form
     {
+        SignalGenerator sine = new SignalGenerator()
+        {
+            Type = SignalGeneratorType.Sin,
+            Gain = 0.2,
+            Frequency = 600
+        };
         public Form1()
         {
             InitializeComponent();
+
+            var player = new WaveOutEvent();
+            player.Init(sine);
+
+            MouseDown += (s, e) => player.Play();
+            MouseUp += (s, e) => player.Stop();
         }
     }
 }
